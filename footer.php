e413d23c5505c56bc5237d0bbbb069e9e4113bc6
DeliaAssoc <jhoroszewski@delianet.com>
2018-04-20 11:36:31 -0400

    Update to footer styling

--- footer.php
+++ footer.php
@@ -15,30 +15,44 @@
 
 	<footer id="colophon" class="site-footer p60">
 
-		<div class="constrain lg">
-			<div class="address">
-				<div class="street text">
-					<?php echo get_theme_mod( 'theme_company_street' ); ?>
+		<div class="constrain lg flexxed">
+			<div class="footer-block left-footer flexxed">
+				<div class="footer-logo">
+					<img src="<?php echo get_theme_mod( 'theme_footer_logo' ); ?>" alt="">
 				</div>
-				<div class="text">
-				<?php echo get_theme_mod( 'theme_company_city' ); ?>, <?php echo get_theme_mod( 'theme_company_state' ); ?> <?php echo get_theme_mod( 'theme_company_zip' ); ?>
-				</div>
-			</div>
-			
-			<div class="contact">
-				<div class="phone text">
-				Phone: <a href="tel:<?php echo get_theme_mod( 'theme_company_phone' ); ?>"><?php echo get_theme_mod( 'theme_company_phone' ); ?></a>
-				</div>	
-				<?php if ( get_theme_mod( 'theme_company_fax' ) ) : ?>
-					<div class="fax text">
-						Fax: <?php echo get_theme_mod( 'theme_company_fax' ); ?>
+				<div class="contact-info">
+					<div class="street text">
+						<?php echo get_theme_mod( 'theme_company_street' ); ?>
 					</div>
-				<?php endif; ?>
-			</div>
+					<div class="city text">
+					<?php echo get_theme_mod( 'theme_company_city' ); ?>, <?php echo get_theme_mod( 'theme_company_state' ); ?> <?php echo get_theme_mod( 'theme_company_zip' ); ?>
+					</div>
+				
+					<div class="phone text">
+					Phone: <a href="tel:<?php echo get_theme_mod( 'theme_company_phone' ); ?>"><?php echo get_theme_mod( 'theme_company_phone' ); ?></a>
+					</div>	
+					<?php if ( get_theme_mod( 'theme_company_fax' ) ) : ?>
+						<div class="fax text">
+							Fax: <?php echo get_theme_mod( 'theme_company_fax' ); ?>
+						</div>
+					<?php endif; ?>
 
-			<div class="copyright">
-				&copy; <?php echo date( 'Y' ); ?> <?php echo bloginfo( 'name' ); ?> All Rights Reserved.
-			</div>
+					<div class="email text">
+						Email: <a href="mailto:<?php echo get_theme_mod( 'theme_email' ); ?>"><?php echo get_theme_mod( 'theme_company_email' ); ?></a>
+					</div>
+				</div>
+			</div><!-- .footer-block .left-footer -->
+			<div class="footer-block right-footer">
+				<div class="copyright text">
+					&copy; <?php echo date( 'Y' ); ?> <?php echo bloginfo( 'name' ); ?>
+				</div>
+				<div class="privacy text">
+					<a href="<?php echo get_theme_mod( 'theme_privacy_policy' ); ?>">Privacy Statement</a>
+				</div>
+				<div class="sitemap text">
+					<a href="<?php echo get_theme_mod( 'theme_sitemap' ); ?>">Site Map</a>
+				</div>
+			</div><!-- .footer-block .right-footer -->
 		</div>
 	
 	</footer><!-- #colophon -->
